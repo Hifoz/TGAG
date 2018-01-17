d8c59fabbb4f5716700a895638b627a54ccd4335
Michael Bråten <braaten.michael@outlook.com>
2018-01-17 21:42:52 +0100

    Moved stored meshes out of meshStorage dict. and placed them inside ChunkData objects, which are stored in chunkStorage in the ChunkManager

---
+++ Assets/Scripts/ChunkData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class ChunkData {
+    private Mesh mesh;
+
+    private Vector3 position;
+
+    ChunkVoxelMesh CVM = new ChunkVoxelMesh();
+
+    /// <summary>
+    /// Construct a new chunk
+    /// </summary>
+    /// <param name="pos">position of the chunk</param>
+    public ChunkData(Vector3 pos) {
+        position = pos;
+
+        mesh = CVM.getVoxelMesh(pos); // This part can be threaded out.
+    }
+
+    public Mesh getMesh() {
+        return mesh;
+    }
+
+}
