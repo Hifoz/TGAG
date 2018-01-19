d1ef11601009fe4d0bda1a4e4a2999eb5453f031
Muff1nz <muffinz314@gmail.com>
2018-01-19 20:12:03 +0100

    Added queues needed for multithreading

---
+++ Assets/Scripts/Utils/BlockingQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+/// <summary>
+/// A blocking queue made by Stephen Toub (Microsoft).
+/// https://blogs.msdn.microsoft.com/toub/2006/04/12/blocking-queues/
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class BlockingQueue<T> {
+    private int count = 0;
+    private Queue<T> queue = new Queue<T>();
+
+    /// <summary>
+    /// Tries to dequeue an item from the queue, blocks if it's empty.
+    /// </summary>
+    /// <returns>An item T from the queue</returns>
+    public T Dequeue() {
+        lock (queue) {
+            while (count <= 0) {
+                Monitor.Wait(queue);
+            }
+
+            count--;
+            return queue.Dequeue();
+        }
+    }
+
+    /// <summary>
+    /// Enqueues an item T to the queue, and wakes up threads blocking on this queue.
+    /// </summary>
+    /// <param name="data">The item T to enqueue</param>
+    public void Enqueue(T data) {
+        if (data == null) throw new ArgumentNullException("data");
+
+        lock (queue) {
+            queue.Enqueue(data);
+            count++;
+            Monitor.Pulse(queue);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/LockingQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+/// <summary>
+/// A locking queue based on a blocking queue made by Stephen Toub (Microsoft).
+/// https://blogs.msdn.microsoft.com/toub/2006/04/12/blocking-queues/
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class LockingQueue<T> {
+    private int count = 0;
+    private Queue<T> queue = new Queue<T>();
+
+    /// <summary>
+    /// Dequeues an item from the queue, will not fail if queue is empty.
+    /// </summary>
+    /// <returns>An item T from the queue</returns>
+    public T Dequeue() {
+        lock (queue) {
+            try {
+                T item = queue.Dequeue();
+                count--;
+                return queue.Dequeue();
+            } catch (InvalidOperationException e) {
+                throw e;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Enqueues the item T to the queue.
+    /// </summary>
+    /// <param name="data">The item to enqueue</param>
+    public void Enqueue(T data) {
+        if (data == null) throw new ArgumentNullException("data");
+
+        lock (queue) {
+            queue.Enqueue(data);
+            count++;
+        }
+    }
+
+    /// <summary>
+    /// Returns the count of the queue.
+    /// </summary>
+    /// <returns>int count</returns>
+    public int getCount() {
+        lock (queue) {
+            lock (count) {
+                return count;
+            }
+        }
+    }
+}
