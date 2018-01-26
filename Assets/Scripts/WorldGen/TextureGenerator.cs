4bc7d3bd8a2e5c2d846b7136098f33260b647e11
Michael Bråten <braaten.michael@outlook.com>
2018-01-26 22:04:27 +0100

    Started working a little bit in a testing scene. Currently only displaying random data on the cubes texture

---
+++ Assets/Scripts/WorldGen/TextureGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureGenerator {
+    Texture2DArray textures;
+    int maxTextures;
+    int size;
+
+    public TextureGenerator(int size, int maxTextures) {
+        this.size = size;
+        this.maxTextures = maxTextures;
+        textures = new Texture2DArray(size, size, maxTextures, TextureFormat.RGBAFloat, false);
+    }
+
+
+    public void GenerateTexture(Color[] pixelData, int index) {
+        textures.SetPixels(pixelData, 0);
+    }
+
+
+    public Texture2DArray GetTextureArray() {
+        return textures;
+    }
+
+    public Color[] GetTexture(int index) {
+        if (index < maxTextures)
+            return textures.GetPixels(index);
+
+        return null;
+    }
+}
---
+++ Assets/TestScenes/TextureGeneration/TextureGenerationTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TextureGenerationTest : MonoBehaviour {
+    Mesh mesh;
+
+
+	// Use this for initialization
+	void Start () {
+        TextureGenerator textureGenerator = new TextureGenerator(512, 2);
+        textureGenerator.GenerateTexture(testData(), 0);
+
+        mesh = GetComponent<MeshFilter>().mesh = new Mesh();
+        GenerateCube();
+
+        Texture2D tex = new Texture2D(512, 512, TextureFormat.RGBAFloat, false);
+        tex.SetPixels(textureGenerator.GetTexture(0));
+        tex.Apply();
+
+        GetComponent<MeshRenderer>().material.SetTexture("_MainTex", tex);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public Color[] testData() {
+        Color[] data = new Color[512*512];
+        for(int i = 0; i < data.Length; i++) {
+            data[i] = new Color(
+                UnityEngine.Random.Range(0f, 1f),
+                UnityEngine.Random.Range(0f, 1f),
+                UnityEngine.Random.Range(0f, 1f)
+                );
+        }
+
+        return data;
+    }
+
+
+    public void GenerateCube() {
+        BlockData[,,] blockdata = new BlockData[,,] {
+            { { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) },
+            { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) },
+            { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) } },
+
+            { { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) },
+            { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.DIRT),new BlockData(BlockData.BlockType.AIR) },
+            { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) } },
+
+            { { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) },
+            { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) },
+            { new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR),new BlockData(BlockData.BlockType.AIR) } },
+        };
+
+
+        MeshData meshData = MeshDataGenerator.GenerateMeshData(blockdata);
+
+        mesh.vertices = meshData.vertices;
+        mesh.triangles = meshData.triangles;
+        mesh.uv = meshData.uvs;
+        mesh.RecalculateNormals();
+    }
+}
