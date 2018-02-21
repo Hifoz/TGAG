d7211c7cba5929170a6d91c688a590012a30383b
Muff1nz <muffinz314@gmail.com>
2018-02-22 00:10:24 +0100

    Added stopwatch class again

---
+++ Assets/Scripts/Utils/StopWatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+/// <summary>
+/// Little wrapper class for the stopwatch, used for debugging / profiling
+/// </summary>
+public class StopWatch {
+
+    Stopwatch stopwatch = new Stopwatch();
+
+    /// <summary>
+    /// Starts the timer
+    /// </summary>
+    public void start() {
+        stopwatch.Start();
+    }
+
+    /// <summary>
+    /// Stops and outputs results to console
+    /// </summary>
+    /// <param name="message">message to add to the output</param>
+    public void done(string message) {
+        stopwatch.Stop();
+        UnityEngine.Debug.Log(string.Format("Stopwatch time: {0}ms __ Custom message: {1}", stopwatch.ElapsedMilliseconds, message));
+        stopwatch.Reset();
+    }
+}
