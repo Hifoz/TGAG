01281dca8d47543425650a49d0fc4a4c97ca5ffe
Michael Bråten <braaten.michael@outlook.com>
2018-02-28 16:21:17 +0100

    Started working on biomes

---
+++ Assets/Scripts/WorldGen/Biomes/Biome.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * WIP:
+ * 
+ * 
+ * All variables are subject to change
+ * 
+ * 
+ * Custom rules for tree generation based on biome? (Currently the rules are stored in LSystemTreeGenerator.rules, and the rule to use is rng)
+ *      Could also affect the texture type, so different foliage types can have different leaves and tree textures?
+ * 
+ * 
+ * Ideas for other biome modifiers:
+ * - Skybox-changes?
+ * - Somehow change the rules for CVDG.decideBlockType()
+ * - 
+ * 
+ * 
+ * 
+ */
+
+
+public class Biome {
+    //General
+    public int snowHeight;
+    //2D noise settings
+    public float frequency2D;
+    public float noiseExponent2D;
+    public int octaves2D;
+    //3D noise settings
+    public float Structure3DRate;
+    public float Unstructure3DRate;
+    public float frequency3D;
+    //Foliage
+    public int maxTreesPerChunk;
+    public float treeLineLength;
+    public float treeVoxelSize;
+    public float treeThickness;
+    public float treeLeafThickness;
+    public int grammarRecursionDepth;
+
+
+    /// <summary>
+    /// Loads a biome from a file
+    /// </summary>
+    /// <param name="filepath">path to file</param>
+    public void loadFromFile(String filepath) {
+        throw new NotImplementedException("Biome.loadFromFile(String filepath) not yet implemented.");
+    }
+}
---
+++ Assets/Scripts/WorldGen/Biomes/BiomeManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class BiomeManager {
+    private List<Biome> biomes;
+    private List<Vector2> biomePoints;
+
+
+
+    BiomeManager() {
+
+    }
+
+    /// <summary>
+    /// Used to update the biomes
+    /// </summary>
+    public void updateBiomes() {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Loads biomes from a folder
+    /// </summary>
+    /// <param name="folderpath">path to folder containing biome files</param>
+    public void loadFromFile(String folderpath) {
+        throw new NotImplementedException("BiomeManager.loadFromFile(String folderpath) not yet implemented.");
+    }
+
+
+
+
+    /// <summary>
+    /// Loads the basic terrain, matching the contents of the ChunkConfig
+    /// </summary>
+    [Obsolete("Only to be used until loadFromFile(..) is implemented")]
+    public void loadBasicType() {
+        Biome basicBiome = new Biome();
+
+        //General
+        basicBiome.snowHeight = 90;
+        //2D noise settings
+        basicBiome.frequency2D = 0.001f;
+        basicBiome.noiseExponent2D = 3;
+        basicBiome.octaves2D = 6;
+        //3D noise settings
+        basicBiome.Structure3DRate = 0.75f;
+        basicBiome.Unstructure3DRate = 0.85f;
+        basicBiome.frequency3D = 0.0075f;
+        //Foliage
+        basicBiome.maxTreesPerChunk = 1;
+        basicBiome.treeLineLength = 2.0f;
+        basicBiome.treeVoxelSize = 1.0f;
+        basicBiome.treeThickness = 0.5f;
+        basicBiome.treeLeafThickness = 3f;
+        basicBiome.grammarRecursionDepth = 4;
+    }
+}
