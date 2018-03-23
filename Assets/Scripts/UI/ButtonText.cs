802342b585c6dc0062a467082f8d37da575a997e
Michael Bråten <braaten.michael@outlook.com>
2018-03-23 17:17:01 +0100

    Started working on giving button-text interaction-animations

---
+++ Assets/Scripts/UI/ButtonText.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// For animating the colors on the text of a button
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class ButtonText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler {
+
+    public Color baseColor;
+    public Color normalColor;
+    public Color highlightedColor;
+    public Color pressedColor;
+    public Color disabledColor;
+
+    Text txt;
+    Button btn;
+    bool interactableDelay;
+
+    void Start() {
+        txt = GetComponentInChildren<Text>();
+        btn = gameObject.GetComponent<Button>();
+        interactableDelay = btn.interactable;
+    }
+
+    void Update() {
+        if (btn.interactable != interactableDelay) {
+            if (btn.interactable) {
+                txt.color = baseColor * normalColor * btn.colors.colorMultiplier;
+            } else {
+                txt.color = baseColor * disabledColor * btn.colors.colorMultiplier;
+            }
+        }
+        interactableDelay = btn.interactable;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (btn.interactable) {
+            txt.color = baseColor * highlightedColor * btn.colors.colorMultiplier;
+        } else {
+            txt.color = baseColor * normalColor * btn.colors.colorMultiplier;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (btn.interactable) {
+            txt.color = baseColor * pressedColor * btn.colors.colorMultiplier;
+        } else {
+            txt.color = baseColor * disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        if (btn.interactable) {
+            txt.color = baseColor * highlightedColor * btn.colors.colorMultiplier;
+        } else {
+            txt.color = baseColor * disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        if (btn.interactable) {
+            txt.color = baseColor * normalColor * btn.colors.colorMultiplier;
+        } else {
+            txt.color = baseColor * disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
+}
