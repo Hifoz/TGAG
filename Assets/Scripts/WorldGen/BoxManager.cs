08ca1f1688e1d7fa898308b4fcef77ca9bc60e43
Muff1nz <muffinz314@gmail.com>
2018-04-10 02:24:19 +0200

    Added box reuse, and reduced amount of boxes used

--- Assets/Scripts/WorldGen/BoxManager.cs
+++ Assets/Scripts/WorldGen/BoxManager.cs
@@ -12,7 +12,8 @@ public class BoxManager : MonoBehaviour {
     private List<GameObject> active = new List<GameObject>();
     private Stack<GameObject> inactive = new Stack<GameObject>();
 
-    private HashSet<Vector3Int> activeMap = new HashSet<Vector3Int>();
+    private HashSet<Vector3> oldGets = new HashSet<Vector3>();
+    private HashSet<Vector3> newGets = new HashSet<Vector3>();
 
     private const int scanArea = 10;
 
@@ -30,19 +31,34 @@ public class BoxManager : MonoBehaviour {
             yield return 0;
         }
         ready = true;
+
+        while (true) {
+            yield return new WaitForSeconds(0);
+            Debug.Log("ACTIVE: " + active.Count + " INACTIVE: " + inactive.Count);
+        }
     }
 
     private void Update() {
         if (ready) {
-            boxForAnimal(worldGenManager.player.gameObject);
-            foreach (GameObjectPool pool in animalPools) {
-                foreach (GameObject animal in pool.activeList) {
-                    boxForAnimal(animal);
-                }
-            }
+            requestBoxes();
+            collect();
         }
     }
 
+
+    public void requestBoxes() {
+        boxForAnimal(worldGenManager.player.gameObject);
+        //foreach (GameObjectPool pool in animalPools) {
+        //    foreach (GameObject animal in pool.activeList) {
+        //        boxForAnimal(animal);
+        //    }
+        //}
+    }
+
+    /// <summary>
+    /// Places boxes for animals
+    /// </summary>
+    /// <param name="animal"></param>
     private void boxForAnimal(GameObject animal) {
         Vector3Int chunkIndex = worldGenManager.world2ChunkPos(animal.transform.position);
         if (worldGenManager.checkBounds(chunkIndex.x, chunkIndex.y) && chunkGrid[chunkIndex.x, chunkIndex.z] != null) {
@@ -52,13 +68,40 @@ public class BoxManager : MonoBehaviour {
                 for (int y = -scanArea / 2; y < scanArea / 2; y++) {
                     for (int z = -scanArea / 2; z < scanArea / 2; z++) {
                         Vector3Int index = Utils.floorVectorToInt(animal.transform.position - chunk.pos + new Vector3(x, y, z));
-                        if (!map.indexEmpty(index)) {
-                            get(Utils.floorVectorToInt(animal.transform.position + new Vector3(x, y, z)), "test", false);
+                        if (/*!map.indexEmpty(index)*/ surfaceVoxel(index, map)) {
+                            get(Utils.floorVector(animal.transform.position + new Vector3(x, y, z)), "test", false);
                         }
                     }
                 }
             }
         }
+    }
+
+    /// <summary>
+    /// Calulates if this is a surface voxel
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="map"></param>
+    /// <returns></returns>
+    private bool surfaceVoxel(Vector3Int index, PremissiveBlockDataMap map) {
+        const int maxNeighbours = 3 * 2;
+        int neighbourCount = 0;
+
+        if (map.indexEmpty(index)) {
+            return false;
+        }
+       
+        for (int axes = 0; axes < 3; axes++) {
+            for (int i = -1; i <= 1; i += 2) {
+                Vector3Int offset = Vector3Int.zero;
+                offset[axes] = i;
+                if (!map.indexEmpty(index + offset)) {
+                    neighbourCount++;
+                }
+            }
+        }
+
+        return neighbourCount != maxNeighbours;
     }
 
     /// <summary>
@@ -68,16 +111,19 @@ public class BoxManager : MonoBehaviour {
     /// <param name="name"></param>
     /// <param name="isTrigger"></param>
     /// <returns></returns>
-    public void get(Vector3Int pos, string name, bool isTrigger) {
-        if (activeMap.Contains(pos))
+    private void get(Vector3 pos, string name, bool isTrigger) {
+        if (oldGets.Contains(pos)) {
+            newGets.Add(pos);
             return;
-        activeMap.Add(pos);
+        }
+        newGets.Add(pos);
         GameObject box;
         if (inactive.Count > 0) {
             box = inactive.Pop();
             initBox(box, pos, name, isTrigger);
         } else {
             box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            box.GetComponent<MeshRenderer>().enabled = false;
             initBox(box, pos, name, isTrigger);
         }
     }
@@ -86,7 +132,18 @@ public class BoxManager : MonoBehaviour {
     /// Makes the pool collect all unused boxes
     /// </summary>
     public void collect() {
+        HashSet<Vector3> temp = oldGets;
+        oldGets = newGets;
+        newGets = temp;
+        newGets.Clear();
 
+        for (int i = 0; i < active.Count; i++) {
+            if (!oldGets.Contains(active[i].transform.position)) {
+                inactive.Push(active[i]);
+                active.RemoveAt(i);
+                i--;
+            }
+        }
     }
 
     /// <summary>
@@ -96,7 +153,7 @@ public class BoxManager : MonoBehaviour {
     /// <param name="pos"></param>
     /// <param name="name"></param>
     /// <param name="isTrigger"></param>
-    private void initBox(GameObject box, Vector3Int pos, string name, bool isTrigger) {
+    private void initBox(GameObject box, Vector3 pos, string name, bool isTrigger) {
         box.transform.position = pos;
         box.name = name;
         box.GetComponent<BoxCollider>().isTrigger = isTrigger;
